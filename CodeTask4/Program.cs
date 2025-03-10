d110f5b60e9ec8488e038ad4451a66a95fe3c73d
@Nigar9597 <@nigarza-bb208@code.edu.az>
2025-03-11 01:27:10 +0400

    first commit

---
+++ CodeTask4/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+#region for loop
+/*namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=1; i<=100; i++)
+            {
+                if(i%2==0)
+                { Console.WriteLine(i); }
+            }
+        }
+    }
+}*/
+#endregion while loop
+
+
+int i = 1;
+while (i <= 100)
+{
+    if (i % 2 == 0)
+        Console.WriteLine(i); i++;
+}
